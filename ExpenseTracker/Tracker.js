7558120111f1bd02549edb7804bc73bf9a381c17
Anusonu24 <anurangu996@gmail.com>
2023-05-03 21:27:20 +0530

    todoapp

---
+++ ExpenseTracker/Tracker.js
@@ -0,0 +1,18 @@
+import React from 'React';
+
+const tracker =  ({track}) =>{
+    return <>{(object.keys(track).length>0)?
+        <div className='container'>
+            <div className='row'>
+                <div className='col m-auto'>
+                    <div className='gutter-gap'>
+                       <table classname='table'>
+                        <thead>
+                           
+                        </thead>
+                       </table>
+                           </div>
+                </div>
+        </div>
+        </>
+}
